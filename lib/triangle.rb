ebe6dc6ea888a2cfe0d39da9e29613daf73a0052
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-17 20:54:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,19 +6,18 @@ class Triangle
   attr_accessor :side1, :side2, :side3
 
   def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
     kind
   end
 
   def kind
-    binding.pry
-    if (side1 == side2 && side2 == side3 && side3 == side1)
-      binding.pry
+    if (@side1 == @side2 && @side2 == @side3 && @side3 == @side1)
       :equilateral
-    elsif (side2 == side3)
-      binding.pry
+    elsif (@side2 == @side3)
       :isosceles
     end
-    binding.pry
   end
 
 end
