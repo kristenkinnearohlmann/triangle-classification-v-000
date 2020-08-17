37157b4a85da83aa055a4847d8135c9e32a98ac3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-17 21:07:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,6 +17,8 @@ class Triangle
       triangle_kind = :equilateral
     elsif (@side2 == @side3 || @side1 == @side3 || @side1 == @side2)
       triangle_kind = :isosceles
+    elsif (@side1 != @side2 && @side2 != @side3 && @side3 != @side1)
+      triangle_kind = :scalene
     end
     triangle_kind
   end
