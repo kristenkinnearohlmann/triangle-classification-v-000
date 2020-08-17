b9d039cb33952b6243de1dd786c77f35d325d4f7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-17 21:01:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,21 +9,15 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    triangle_kind = kind
-    binding.pry
-    triangle_kind
+    kind
   end
 
   def kind
-    binding.pry
     if (@side1 == @side2 && @side2 == @side3 && @side3 == @side1)
-      binding.pry
       triangle_kind = :equilateral
     elsif (@side2 == @side3)
-      binding.pry
       triangle_kind = :isosceles
     end
-    binding.pry
     triangle_kind
   end
 
